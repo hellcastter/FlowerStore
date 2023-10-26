0b44341c71d4281ebb28e655157a3d44fe1074d2
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-10-26 18:29:11 +0300

    fixed superlinter

--- src/main/java/ua/edu/ucu/apps/flowerstore/FlowerStoreApplication.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/FlowerStoreApplication.java
@@ -3,10 +3,9 @@ package ua.edu.ucu.apps.flowerstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FlowerStoreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerStoreApplication.class, args);
+    }
 }
